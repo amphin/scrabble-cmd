79641fca6b10f8bb616525c31578d297fa73a7dd
Abieshek <trainee.abieshek.prabakaran@sigmalabs.co.uk>
2026-04-08 16:06:47 +0100

    Player menu progress

---
+++ display.py
@@ -0,0 +1,45 @@
+def print_title():
+    print(r"""                             __       __       ___                                      __     
+                            /\ \     /\ \     /\_ \                                    /\ \    
+  ____    ___   _ __    __  \ \ \____\ \ \____\//\ \      __         ___    ___ ___    \_\ \   
+ /',__\  /'___\/\`'__\/'__`\ \ \ '__`\\ \ '__`\ \ \ \   /'__`\      /'___\/' __` __`\  /'_` \  
+/\__, `\/\ \__/\ \ \//\ \L\.\_\ \ \L\ \\ \ \L\ \ \_\ \_/\  __/     /\ \__//\ \/\ \/\ \/\ \L\ \ 
+\/\____/\ \____\\ \_\\ \__/.\_\\ \_,__/ \ \_,__/ /\____\ \____\    \ \____\ \_\ \_\ \_\ \___,_\
+ \/___/  \/____/ \/_/ \/__/\/_/ \/___/   \/___/  \/____/\/____/     \/____/\/_/\/_/\/_/\/__,_ /)
+""")
+
+
+def print_board(board):
+    print("      1  2  3  4  5  6  7  8  9  10 11 12 13 14 15")
+    print("      |  |  |  |  |  |  |  |  |  |  |  |  |  |  |")
+
+    for i in range(len(board[0])):
+        if i < 9:
+            print(" ", end="")
+        print(f"{i+1} - ", end="")
+
+        for j in range(len(board[i])):
+            print(f"[{board[i][j]}]", end="")
+        print()
+
+
+# TODO: center text based on player name length
+def print_player_turn_start(player):
+    print("--------------------------------------------------------")
+    print(f"                    {player.upper()}'S TURN                    ")
+    print("--------------------------------------------------------")
+
+
+def print_player_tiles(tiles, points):
+    print("              ", end="")
+    for tile in tiles:
+        print(tile, end="   ")
+
+    print()
+    print("              ", end="")
+    for point in points:
+        print(point, end="  ")
+        if point < 10:
+            print(" ", end="")
+
+    print()
--- main.py
+++ main.py
@@ -1,4 +1,90 @@
-print("Welcome to Scrabble")
+import sys
+import random
+import display
+import game_pieces as gp
+
+
+def player_menu():
+    print("""\n1. Place word
+2. Exchange letters
+3. Shuffle letters (no turn spent)\n""")
+    while True:
+        choice = input("> ")
+        if choice == "1" or choice == "2" or choice == "3":
+            return choice
+        else:
+            sys.stdout.write("\033[F\033[K")
+
+
+def exchange_letters_prompt(letters):
+    print("Enter letter to exchange (and . to confirm selection)")
+    letters_selected = ""
+    while True:
+        print(letters_selected)
+        letter = input("> ")
+        if letter.upper() in letters:
+            letters_selected += letter.upper() + " "
+            letters.remove(letter.upper())
+        elif letter == ".":
+            return letters_selected.split(" ")
+
+        sys.stdout.write("\033[F\033[K")
+        sys.stdout.write("\033[F\033[K")
+
+
+display.print_title()
 
 player1_name = input("Enter name of Player 1: ")
 player2_name = input("Enter name of Player 2: ")
+player_names = [player1_name, player2_name]
+num_players = 2  # TODO: dynamic between 2-4 players
+
+# print rules
+
+bag = []
+for i in range(len(gp.letter_tiles)):
+    bag += [gp.letter_tiles[i]] * gp.letters_in_bag[i]
+
+players_tiles = [[], []]
+scores = [0, 0]
+turn = 0
+
+for i in range(7):
+    for player_tiles in players_tiles:
+        player_tile = random.choice(bag)
+        bag.remove(player_tile)
+        player_tiles.append(player_tile)
+
+# game loop
+while len(bag) > 0:
+    display.print_player_turn_start(player_names[turn])
+    display.print_board(gp.board)
+    print()
+
+    tile_points = []
+    for tile in players_tiles[turn]:
+        tile_points.append(gp.letter_points[tile])
+    display.print_player_tiles(players_tiles[turn], tile_points)
+
+    choice = player_menu()
+    # method to handle choice and loop menu accordingly
+
+    while choice == "2":
+        letters_to_exchange = exchange_letters_prompt(players_tiles[turn])
+        # exchange letters and display to user
+        choice = player_menu()
+
+    while choice == "3":
+        random.shuffle(players_tiles[turn])
+        for i in range(8):
+            sys.stdout.write("\033[F\033[K")
+
+        tile_points = []
+        for tile in players_tiles[turn]:
+            tile_points.append(gp.letter_points[tile])
+        display.print_player_tiles(players_tiles[turn], tile_points)
+
+        choice = player_menu()
+    # turn = (turn + 1) % num_players
+
+    # prompt to begin next player turn
